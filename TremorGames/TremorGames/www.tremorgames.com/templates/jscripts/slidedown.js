25853359245a6170325bef995c49a829fe145bbc
xelus47 <aleksei0kozlov@gmail.com>
2016-07-16 21:19:49 +0200

    js source

---
+++ TremorGames/TremorGames/www.tremorgames.com/templates/jscripts/slidedown.js
@@ -0,0 +1,9 @@
+var slideDownInitHeight=new Array();var slidedown_direction=new Array();var slidedownActive=false;var contentHeight=false;var slidedownSpeed=3;var slidedownTimer=7;function slidedown_showHide(boxId)
+{if(!slidedown_direction[boxId])slidedown_direction[boxId]=1;if(!slideDownInitHeight[boxId])slideDownInitHeight[boxId]=0;if(slideDownInitHeight[boxId]==0)slidedown_direction[boxId]=slidedownSpeed;else slidedown_direction[boxId]=slidedownSpeed*-1;slidedownContentBox=document.getElementById(boxId);var subDivs=slidedownContentBox.getElementsByTagName('DIV');for(var no=0;no<subDivs.length;no++){if(subDivs[no].className=='dhtmlgoodies_content')slidedownContent=subDivs[no];}
+contentHeight=slidedownContent.offsetHeight;slidedownContentBox.style.visibility='visible';slidedownActive=true;slidedown_showHide_start(slidedownContentBox,slidedownContent);}
+function slidedown_showHide_start(slidedownContentBox,slidedownContent)
+{if(!slidedownActive)return;slideDownInitHeight[slidedownContentBox.id]=slideDownInitHeight[slidedownContentBox.id]/1+ slidedown_direction[slidedownContentBox.id];if(slideDownInitHeight[slidedownContentBox.id]<=0){slidedownActive=false;slidedownContentBox.style.visibility='hidden';slideDownInitHeight[slidedownContentBox.id]=0;}
+if(slideDownInitHeight[slidedownContentBox.id]>contentHeight){slidedownActive=false;}
+slidedownContentBox.style.height=slideDownInitHeight[slidedownContentBox.id]+'px';slidedownContent.style.top=slideDownInitHeight[slidedownContentBox.id]- contentHeight+'px';setTimeout('slidedown_showHide_start(document.getElementById("'+ slidedownContentBox.id+'"),document.getElementById("'+ slidedownContent.id+'"))',slidedownTimer);}
+function setSlideDownSpeed(newSpeed)
+{slidedownSpeed=newSpeed;}
